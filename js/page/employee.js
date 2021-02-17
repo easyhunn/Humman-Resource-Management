830e46a02d3e393997593fad4869256d86429b65
easyhunn <Hungvugl123@gmail.com>
2021-02-18 00:42:49 +0700

    first commit

---
+++ js/page/employee.js
@@ -0,0 +1,25 @@
+﻿$(document).ready(function () {
+    loadData();
+})
+
+//load employee data
+
+function loadData() {
+    $.ajax({
+        url: "http://api.manhnv.net/api/employees",
+        method: "GET"
+    }).done(function (res) {
+        data = res;
+        $.each(data, function (i, item) {
+            var tr = $(`<tr></tr>`);
+            $.each($("th"), function (index, d) {
+                var filedName = $(d).attr("fieldName");
+                var td = $(`<td>${item[filedName]}</td>`);
+                tr.append(td);
+            })
+            $("table tbody").append(tr);
+        })
+    }).fail(function (err) {
+
+    })
+}
